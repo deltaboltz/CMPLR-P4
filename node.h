b5fc2c7254fa436fa7112527fb4e5d26b35d5809
Connor Schultz <deltaboltz@me.com>
2021-05-04 13:34:17 -0500

    seg fault fix?

--- node.h
+++ node.h
@@ -352,19 +352,19 @@ void node<T>::genASM(std::ostream& out, int scope, std::set<std::string>& varset
 		// ACC: a-b
 		if (tokens_.size())
     { // < | > | =
-			if (tokens_[3].instance == "=<")
+			if (tokens_[4].instance == "=<")
       {
 				out << "BRZPOS ";
 			}
-      else if (tokens_[3].instance == "=>")
+      else if (tokens_[4].instance == "=>")
       {
 				out << "BRZNEG ";
 			}
-      else if(tokens_[3].instance == "==")
+      else if(tokens_[4].instance == "==")
       {
 				out << "BRZERO ";
 			}
-      else if(tokens_[3].instance == "[")
+      else if(tokens_[4].instance == "[")
       {
         out << "BRPOS " << labelctr << "\n";
         out << "BRNEG ";
@@ -401,19 +401,19 @@ void node<T>::genASM(std::ostream& out, int scope, std::set<std::string>& varset
 		// ACC: a-b
 		if (tokens_.size())
     { // < | > | =
-      if (tokens_[3].instance == "=<")
+      if (tokens_[4].instance == "=<")
       {
 				out << "BRZPOS ";
 			}
-      else if (tokens_[3].instance == "=>")
+      else if (tokens_[4].instance == "=>")
       {
 				out << "BRZNEG ";
 			}
-      else if(tokens_[3].instance == "==")
+      else if(tokens_[4].instance == "==")
       {
 				out << "BRZERO ";
 			}
-      else if(tokens_[3].instance == "[")
+      else if(tokens_[4].instance == "[")
       {
         out << "BRPOS " << labelctr << "\n";
         out << "BRNEG ";
