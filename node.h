5b797273b2bdca10db6500ca9e1f49ea4e00de27
Connor Schultz <deltaboltz@me.com>
2021-05-05 00:33:24 -0500

    replaced check

--- node.h
+++ node.h
@@ -561,7 +561,7 @@ void node<T>::setR0Call(std::ostream& out, int scope, std::set<std::string>& var
     out << "MULT mathvar\n";
     out << "BRPOS " << endLabel << "\n";
   }
-  else
+  else if(!tokens_[2].instance.compare("]"))
   {
     out << "THIS IS A TEST FOR [ == ] !!!!\n";
     out << "SUB mathvar\n";
