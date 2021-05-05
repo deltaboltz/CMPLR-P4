ff8eb85d35354fb8470b4929d1408940d749621d
Connor Schultz <deltaboltz@me.com>
2021-05-05 00:10:58 -0500

    Update to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
  * Date Made: 04/15/2021
  * Files Needed: any file with extension .sp21 & makefile to compile all files
  * Made For: CS-4280 (Program Translation Project) @ UMSL
- * Last Update : 05/04/2021 - Added more code to node.h and various bug fixes
+ * Last Update : 05/04/2021 - Added comments and made code more readable
  * TODO : FINISHED
  */
 
