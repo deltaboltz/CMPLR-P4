2064d662cae6469d7b37b742662c20582fa24146
Connor Schultz <deltaboltz@me.com>
2021-05-05 12:11:16 -0500

    Subtract one

--- node.h
+++ node.h
@@ -479,7 +479,7 @@ void node<T>::genASM(std::ostream& out, int scope, std::set<std::string>& varset
 		//gen asm for stat
 		children_[children_.size()-1].genASM(out, scope, varset, stackvars, labelctr);
 
-    std::string oldLabel(labelctr.c_str());
+    std::string oldLabel(labelctr.c_str() - 1);
 		out << "BR " << oldLabel << "\n";
 		out << endLabelHolder << ": NOOP\n";
 
